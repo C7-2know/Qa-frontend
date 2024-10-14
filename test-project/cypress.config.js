c0f7f0d1a78dcff7df9f6ad30f16cb8c46d172c5
c7-2know <betelhemyimamd@gmail.com>
2024-10-14 16:22:59 +0300

    update cypress config

--- test-project/cypress.config.js
+++ test-project/cypress.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "cypress";
-const { installPlugin } = require("@chromatic-com/cypress");
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      installPlugin(on, config);
       // implement node event listeners here
     },
     experimentalStudio: true,
